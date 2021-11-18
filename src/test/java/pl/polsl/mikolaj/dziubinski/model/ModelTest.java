910159a182c8582f16c65869c7e847d20327874f
MikolajDzi <mikolaj.dziubinski@interia.pl>
2021-11-18 12:43:00 +0100

    Modified comments

--- src/test/java/pl/polsl/mikolaj/dziubinski/model/ModelTest.java
+++ src/test/java/pl/polsl/mikolaj/dziubinski/model/ModelTest.java
@@ -106,7 +106,7 @@ public class ModelTest {
     }
     
     /**
-     * Stream method to pass values to unit parametrized unit tests
+     * Stream method to pass values to parametrized unit tests
      * 
      * @return stream of arguments consisting of Roman numeral of type string and Arabic number of type int
      */
